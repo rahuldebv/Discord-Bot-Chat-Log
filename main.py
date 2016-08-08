aca0ef950bfe9c7d2d4fa74ba9b89bef233b1414
Leif Coleman <mageleif@yahoo.com>
2016-08-08 14:43:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from discord.ext import commands
 from chatlog import ChatLog
 import configparser
 
+
+def token():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    return config.get("bot", "token")
+
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"))
 bot.add_cog(ChatLog(bot))
 
@@ -10,6 +16,4 @@ bot.add_cog(ChatLog(bot))
 async def on_ready():
     print(bot.user.name + "#" + bot.user.discriminator + " is now running!")
 
-config = configparser.ConfigParser()
-config.read("config.ini")
-bot.run(config.get("bot", "token"))
+bot.run(token())
